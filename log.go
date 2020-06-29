c499e0db96350db6c306c18264864e7d2ffb50fc
bingoo <bingoo.huang@gmail.com>
2020-06-29 22:50:37 +0800

    Composite stores

--- log.go
+++ log.go
@@ -100,3 +100,20 @@ func NewLogrusStore() *LogrusStore {
 func (s *LogrusStore) Store(log *Log) {
 	logrus.Infof("http:%+v\n", log)
 }
+
+// Stores is the composite stores.
+type Stores struct {
+	Composite []Store
+}
+
+// Store stores the log in database like MySQL, InfluxDB, and etc.
+func (s *Stores) Store(log *Log) {
+	for _, v := range s.Composite {
+		v.Store(log)
+	}
+}
+
+// NewStores composes the stores as a Store
+func NewStores(stores ...Store) *Stores {
+	return &Stores{Composite: stores}
+}
