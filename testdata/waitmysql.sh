68850ce62d5ecb36fbf5a251f90e19a7d76cfb9e
bingoohuang <bingoo.huang@gmail.com>
2020-06-29 15:24:30 +0800

    ci

--- testdata/waitmysql.sh
+++ testdata/waitmysql.sh
@@ -3,10 +3,9 @@ echo Starting MySQL...
 
 status="1"
 while [ "$status" != "0" ]; do
-  sleep 1
-  mysql -e 'select version()'
+  sleep 5
+  mysql -e 'select VERSION()'
   status=$?
 done
 
 mysql -e 'select VERSION()'
-
